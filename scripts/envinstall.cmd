f609d43148033d8b852e4c82b31775e9b689f7d4
chxadr <adrien.chevrier37@gmail.com>
2025-12-23 14:46:10 +0100

    pyproject.toml and envinstall.cmd script corrected

--- scripts/envinstall.cmd
+++ scripts/envinstall.cmd
@@ -1,48 +1,43 @@
+:: NOTE : this script relies on Python install manager.
+::        see https://www.python.org/downloads/windows/
+
 @echo off
 setlocal enabledelayedexpansion
 
-set VENV_DIR=.venv
-set PY_VERSION=3.13
+set "VENV_DIR=.venv"
+set "PY_VERSION=3.13"
 
-color 0A
 echo Checking Python %PY_VERSION% version requirements ...
 
 :: Check Python version
 py -V:%PY_VERSION% --version >nul 2>&1
 if %ERRORLEVEL% neq 0 (
-    color 0A
     echo Python %PY_VERSION% not found.
     echo Attempting to install Python %PY_VERSION% ...
     py install %PY_VERSION%   
     if %ERRORLEVEL% neq 0 (
-       color 0C
-       echo Failed to install Python %PY_VERSION%. Please install it manually
-       exit /b 1
+        echo Failed to install Python %PY_VERSION%. Please install it manually
+        exit /b 1
     )
-    color 0A
     echo Python %PY_VERSION% installed successfully
 )
 :: Create the virtual environment
-color 0A
 echo Creating Python virtual environment ...
 echo     Python version: %PY_VERSION%
-echo     Location: %VENV_DIR%
+echo     Location: %VENV_DIR%%NC%
 py -V:%PY_VERSION% -m venv %VENV_DIR%
 if %ERRORLEVEL% neq 0 (
-    color 0C
     echo Failed to create Python virtual environment
     if exist "%VENV_DIR%" (
         rmdir /s /q "%VENV_DIR%"
     )
     exit /b 1
 )
-color 0A
-echo Python virtual environment created at %VENV_DIR%
+echo Python virtual environment created at %VENV_DIR%%NC%
 
 :: Install requirements
 %VENV_DIR%\Scripts\pip install -r requirements.txt
 if %ERRORLEVEL% neq 0 (
-    color 0C
     echo Failed to install requirements
     echo Consider a manual installation of all dependencies
     echo     run: %VENV_DIR%\Scripts\activate
@@ -50,7 +45,6 @@ if %ERRORLEVEL% neq 0 (
     echo Or delete %VENV_DIR% with rmdir /s /q %VENV_DIR%
     exit /b 1
 )
-color 0A
 echo Python virtual environment is ready to be used
 echo To activate, run: %VENV_DIR%\Scripts\activate
 exit /b 0
